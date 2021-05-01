63e6172c2bb00342ccb079694a741d34ca62b85e
vbaobao <vbao.main@gmail.com>
2021-04-30 20:57:26 -0700

    write Create and rewrite Read endpoints

--- server/api.js
+++ server/api.js
@@ -1,14 +1,25 @@
+require('../db/database.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const db = require('../db/index.js');
+const instructorsInsert = require('../db/inserters/instructorsInserter.js');
+const offeredBysInsert = require('../db/inserters/offeredBysInserter.js');
+const testimonialsInsert = require('../db/inserters/testimonialsInserter.js');
+const { getAllInstructors, getInstructors, getPrimaryInstructor } = require('../db/getters/instructorsGetters.js');
+const { getAllOfferedBys, getOfferedBys } = require('../db/getters/offeredBysGetters.js');
+const getTestimonials = require('../db/getters/testimonialsGetters.js');
+const setInstructor = require('../db/setters/instructorsSetters.js');
+const setOfferedBy = require('../db/setters/offeredBysSetters.js');
+const setTestimonial = require('../db/setters/testimonialsSetters.js');
+const deleteInstructor = require('../db/deleters/instructorsDeleters.js');
+const deleteOfferedBy = require('../db/deleters/offeredBysDeleters.js');
+const deleteTestimonial = require('../db/deleters/testimonialsDeleters.js');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
 
@@ -16,9 +27,32 @@ app.get('/:courseNumber', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
+// CREATE
+// Expects an array of instructor objects
+app.post('/api/addinstructors', (req, res) => {
+  instructorsInsert(req.body)
+    .then((dbResponse) => res.send(dbResponse))
+    .catch((err) => res.send(err).status(400));
+});
+
+// Expects an array of offeredBy objects
+app.post('/api/addofferedbys', (req, res) => {
+  offeredBysInsert(req.body)
+    .then((dbResponse) => res.send(dbResponse))
+    .catch((err) => res.send(err).status(400));
+});
+
+// Expects an array of testimonial objects
+app.post('/api/addtestimonals', (req, res) => {
+  testimonialsInsert(req.body)
+    .then((dbResponse) => res.send(dbResponse))
+    .catch((err) => res.send(err).status(400));
+});
+
+// READ
 // returns all instructors documents
 app.get('/api/allinstructors', (req, res) => {
-  db.findAllInstructors((err, dbResponse) => {
+  getAllInstructors((err, dbResponse) => {
     if (err) {
       res.send(err).status(400);
     } else {
@@ -29,21 +63,21 @@ app.get('/api/allinstructors', (req, res) => {
 
 // returns an array of instructors that belong to a course
 app.get('/api/instructors/:courseNumber', (req, res) => {
-  db.findInstructors(parseInt(req.params.courseNumber, 10), (dbResponse) => {
+  getInstructors(parseInt(req.params.courseNumber, 10), (dbResponse) => {
     res.send(dbResponse);
   });
 });
 
 // returns the primary instructor for a course
 app.get('/api/primaryInstructor/:courseNumber', (req, res) => {
-  db.findPrimaryInstructor(parseInt(req.params.courseNumber, 10), (dbResponse) => {
+  getPrimaryInstructor(parseInt(req.params.courseNumber, 10), (dbResponse) => {
     res.send(dbResponse);
   });
 });
 
 // returns all offeredBy documents for all courses
 app.get('/api/offeredByAll', (req, res) => {
-  db.findAllOfferedBys((err, dbResponse) => {
+  getAllOfferedBys((err, dbResponse) => {
     if (err) {
       res.send(err).status(400);
     } else {
@@ -54,16 +88,42 @@ app.get('/api/offeredByAll', (req, res) => {
 
 // returns the offeredBy for a course
 app.get('/api/offeredBy/:courseNumber', (req, res) => {
-  db.findOfferedBy(parseInt(req.params.courseNumber, 10), (dbResponse) => {
+  getOfferedBys(parseInt(req.params.courseNumber, 10), (dbResponse) => {
     res.send(dbResponse);
   });
 });
 
 // returns three random testimonials
 app.get('/api/testimonials/:courseNumber', (req, res) => {
-  db.findTestimonials(parseInt(req.params.courseNumber, 10), (dbResponse) => {
+  getTestimonials(parseInt(req.params.courseNumber, 10), (dbResponse) => {
     res.send(dbResponse);
   });
+});
+
+// UPDATE
+app.put('', (req, res) => {
+  test
+});
+
+app.put('', (req, res) => {
+  test
+});
+
+app.put('', (req, res) => {
+  test
+});
+
+// DELETE
+app.delete('', (req, res) => {
+  test
+});
+
+app.delete('', (req, res) => {
+  test
+});
+
+app.delete('', (req, res) => {
+  test
 });
 
 module.exports = app;
