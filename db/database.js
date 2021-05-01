ea6dab42e93f6e585a79113c1200e76ced028db8
vbaobao <vbao.main@gmail.com>
2021-05-01 00:58:23 -0700

    debug  env values

--- db/database.js
+++ db/database.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.DATABASE}/coursera`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log(`The database has connected on mongodb://${process.env.DATABASE}/coursera`);
+  console.log(`The database has connected on mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`);
 });
