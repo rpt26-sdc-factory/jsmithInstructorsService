dc57b5009209dcab23c2a781fb6adce36b1eaf0e
vbaobao <vbao.main@gmail.com>
2021-04-30 20:56:58 -0700

    remove unused method

--- db/getters/testimonialsGetters.js
+++ db/getters/testimonialsGetters.js
@@ -1,19 +1,10 @@
 const Promise = require('bluebird');
 const { TestimonialsModel } = require('../models.js');
 
-const getAllTestimonials = Promise.promisify((courseIds, cb) => {
-  TestimonialsModel.find()
-    .then((results) => cb(null, results))
-    .catch((err) => cb(err));
-});
-
 const getTestimonials = Promise.promisify((courseIds, cb) => {
   TestimonialsModel.find({ _id: { $in: courseIds } })
     .then((results) => cb(null, results))
     .catch((err) => cb(err));
 });
 
-module.exports = {
-  getAllTestimonials,
-  getTestimonials,
-};
+module.exports = getTestimonials;
