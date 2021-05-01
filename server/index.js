e914c0f7127d91bc5924b2c71891137dada4e402
vbaobao <vbao.main@gmail.com>
2021-05-01 13:28:34 -0700

    add env functionality

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 /* eslint-disable radix */
+require('dotenv').config();
 const app = require('./api.js');
 
-const port = 3003;
+const port = process.env.SERVER_PORT;
 
 app.listen(port, () => {
   console.log(`Instructors service listening at http://localhost:${port}`);
