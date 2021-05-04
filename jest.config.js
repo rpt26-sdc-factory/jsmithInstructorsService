98a15222f6fe0731f32896a693b69f0776650802
vbaobao <vbao.main@gmail.com>
2021-05-04 01:09:52 -0700

    ignore old tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 module.exports = {
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ['/db/data/data.test.js'],
   coveragePathIgnorePatterns: ['/node_modules/'],
   collectCoverageFrom: [
     '**/server/*.{js,jsx}',
