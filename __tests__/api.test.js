7b8965a9823889a751887562350ff671c2d76b3b
vbaobao <vbao.main@gmail.com>
2021-05-04 01:10:17 -0700

    functional test environment

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,26 +2,37 @@ require('dotenv').config();
 const supertest = require('supertest');
 const app = require('../server/api.js');
 const mongoose = require('mongoose');
+const { InstructorsModel, OfferedBysModel, TestimonialsModel } = require('../db/models.js');
 const { instructorsInsert, offeredBysInsert, testimonialsInsert } = require('../db/methods.js');
 const instructorsData = require('../db/data/instructors.json');
-const offeredBysData = require('../db/data/offeredByes.json');
+const offeredBysData = require('../db/data/offeredBys.json');
 const testimonialsData = require('../db/data/testimonials.json');
 
 const request = supertest(app);
 
-app.listen(process.env.SERVER_PORT);
-
 mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/test`, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-beforeAll(() => {
-  // Seed test DB
+beforeAll(async (done) => {
+  await InstructorsModel.remove({});
+  await OfferedBysModel.remove({});
+  await TestimonialsModel.remove({});
+  await instructorsInsert(instructorsData);
+  await offeredBysInsert(offeredBysData);
+  await testimonialsInsert(testimonialsData);
+  done();
 });
 
-afterAll(() => {
-  mongoose.connection.close();
+afterAll(async (done) => {
+  await InstructorsModel.remove({});
+  await OfferedBysModel.remove({});
+  await TestimonialsModel.remove({});
+  await mongoose.connection.close();
+  done();
 });
 
 describe('test chunk', () => {
-  test('test1', () => {});
+  test('test1', () => {
+    expect(true).toEqual(true);
+  });
 });
