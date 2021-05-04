6a5e43a2bb68e3ce9ddf2a85c01788c662b8eb7a
vbaobao <vbao.main@gmail.com>
2021-05-04 01:12:27 -0700

    import db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable radix */
 require('dotenv').config();
+require('../db/database.js');
 const app = require('./api.js');
 
 const port = process.env.SERVER_PORT;
