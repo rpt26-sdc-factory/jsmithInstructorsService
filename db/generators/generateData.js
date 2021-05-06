ae84966f80ad9a14f174f71fd0c0a12565370af8
vbaobao <vbao.main@gmail.com>
2021-05-05 21:21:17 -0700

    move fnction parameter to export files

--- db/generators/generateData.js
+++ db/generators/generateData.js
@@ -2,13 +2,8 @@ const generateInstructors = require('./instructorsGenerator');
 const generateOfferedBys = require('./offeredBysGenerator');
 const generateTestimonials = require('./testimonialsGenerator');
 
-const totalRecords = 10000000;
-const instructors = generateInstructors(totalRecords);
-const offeredBys = generateOfferedBys(totalRecords);
-const testimonials = generateTestimonials(totalRecords);
-
 module.exports = {
-  instructors,
-  offeredBys,
-  testimonials,
+  generateInstructors,
+  generateOfferedBys,
+  generateTestimonials,
 };
