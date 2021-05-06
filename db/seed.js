bef1d8fe940a42dc14d1c08958447b8c296c5f70
vbaobao <vbao.main@gmail.com>
2021-05-05 21:19:33 -0700

    changed data to data generation function

--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { instructorsInsert, offeredBysInsert, testimonialsInsert } = require('./methods.js');
-const { instructors, offeredBys, testimonials } = require('./generators/generateData.js');
+const { generateInstructors, generateOfferedBys, generateTestimonials } = require('../db/generators/generateData.js');
 
 const seed = async () => {
   mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -12,6 +12,11 @@ const seed = async () => {
   db.once('open', () => {
     console.log(`The database has connected on mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`);
   });
+
+  const initialRecords = 10000;
+  const instructors = generateInstructors(initialRecords);
+  const offeredBys = generateOfferedBys(initialRecords);
+  const testimonials = generateTestimonials(initialRecords);
 
   await instructorsInsert(instructors)
     .then(() => console.log('Inserted instructors.'))
