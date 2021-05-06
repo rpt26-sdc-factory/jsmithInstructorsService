184b3b12e7891dbaba6c920cbfc7788db3c03d36
vbaobao <vbao.main@gmail.com>
2021-05-05 21:19:06 -0700

    modified data to data functions

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -5,14 +5,17 @@ const mongoose = require('mongoose');
 const app = require('../server/api.js');
 const { InstructorsModel, OfferedBysModel, TestimonialsModel } = require('../db/models.js');
 const { instructorsInsert, offeredBysInsert, testimonialsInsert } = require('../db/methods.js');
-const instructorsData = require('../db/data/instructors.json');
-const offeredBysData = require('../db/data/offeredBys.json');
-const testimonialsData = require('../db/data/testimonials.json');
+const { generateInstructors, generateOfferedBys, generateTestimonials } = require('../db/generators/generateData.js');
 
 const request = supertest(app);
 
 beforeAll(async (done) => {
   mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/test`, { useNewUrlParser: true, useUnifiedTopology: true });
+  const initialRecords = 100;
+  const instructorsData = generateInstructors(initialRecords);
+  const offeredBysData = generateOfferedBys(initialRecords);
+  const testimonialsData = generateTestimonials(initialRecords);
+
   await InstructorsModel.deleteMany({});
   await OfferedBysModel.deleteMany({});
   await TestimonialsModel.deleteMany({});
