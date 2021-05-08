1b28cdc68df292a4f35c11a0a7c865b7362965c1
vbaobao <vbao.main@gmail.com>
2021-05-08 11:59:52 -0700

    working batch seeding script

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,24 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { instructorsInsert, offeredBysInsert, testimonialsInsert } = require('./methods.js');
 const { generateInstructors, generateOfferedBys, generateTestimonials } = require('./generators/generateData.js');
 
-const seed = async () => {
+const batch = async (name, dataFunction, insertFunction, batchSize, numBatches) => {
+  const startTimer = new Date();
+  const data = dataFunction(batchSize);
+  await insertFunction(data)
+    .then(() => {
+      const endTimer = new Date();
+      console.log(`Inserted ${batchSize} ${name}'s: ${endTimer - startTimer} seconds. ${numBatches - 1} remaining.`);
+      if (numBatches > 1) batch(name, dataFunction, insertFunction, batchSize, numBatches--);
+    })
+    .catch((err) => console.error(err));
+};
+
+const seed = async (batchSize, numBatches) => {
   mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`, { useNewUrlParser: true, useUnifiedTopology: true });
 
   const db = mongoose.connection;
@@ -13,24 +27,11 @@ const seed = async () => {
     console.log(`The database has connected on mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/coursera`);
   });
 
-  const initialRecords = 10000;
-  const instructors = generateInstructors(initialRecords);
-  const offeredBys = generateOfferedBys(initialRecords);
-  const testimonials = generateTestimonials(initialRecords);
+  await batch('Instructor', generateInstructors, instructorsInsert, batchSize, numBatches);
+  await batch('OfferedBy', generateOfferedBys, offeredBysInsert, batchSize, numBatches);
+  await batch('Testimonial', generateTestimonials, testimonialsInsert, batchSize, numBatches);
 
-  await instructorsInsert(instructors)
-    .then(() => console.log('Inserted instructors.'))
-    .catch((err) => console.error(err));
-
-  await offeredBysInsert(offeredBys)
-    .then(() => console.log('Inserted offeredBys.'))
-    .catch((err) => console.error(err));
-
-  await testimonialsInsert(testimonials)
-    .then(() => console.log('Inserted testimonials.'))
-    .catch((err) => console.error(err));
-
-  db.close();
+  // db.close();
 };
 
-seed();
+seed(100, 1);
