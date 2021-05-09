706262a2869a6e79bcd27f1874fe65ada8220603
vbaobao <vbao.main@gmail.com>
2021-05-09 15:27:23 -0700

    add writing to file, randomize num records

--- db/generators/testimonialsGenerator.js
+++ db/generators/testimonialsGenerator.js
@@ -1,36 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
+require('dotenv').config();
 const faker = require('faker');
-// const fs = require('fs');
+const fs = require('fs');
 
-// creates 9 testimonials
-const generateTestimonials = (courses) => {
+const generateTestimonials = (courses, filenum) => {
   const testimonials = [];
 
   for (let id = 1; id <= courses; id++) {
-    const testimonial = [
-      {
+    const random = Math.floor(Math.random() * 5);
+    for (let reviews = 0; reviews <= random; reviews++) {
+      const testimonial = {
         courseNumber: id,
         name: faker.name.firstName().concat(' ', faker.name.lastName().slice(0, 1), '.'),
         testimonialText: faker.lorem.paragraph(),
-      },
-      {
-        courseNumber: id,
-        name: faker.name.firstName().concat(' ', faker.name.lastName().slice(0, 1), '.'),
-        testimonialText: faker.lorem.paragraph(),
-      },
-      {
-        courseNumber: id,
-        name: faker.name.firstName().concat(' ', faker.name.lastName().slice(0, 1), '.'),
-        testimonialText: faker.lorem.paragraph(),
-      },
-    ];
-    testimonials.push(...testimonial);
+      };
+      testimonials.push(testimonial);
+    }
   }
 
-  // fs.writeFileSync('./db/data/testimonials.json', JSON.stringify(testimonials, null, '\t'));
+  fs.writeFileSync(`./db/seeders/testimonials_${filenum}.json`, JSON.stringify(testimonials, null, '\t'));
   return testimonials;
 };
+
+let count = 20;
+const start = new Date();
+while (count > 0) {
+  generateTestimonials(process.env.PRIMARY_RECORD_BATCH_SIZE / 2, count);
+  count--;
+}
+const end = new Date();
+console.log('Time to complete: ', end - start, 'ms');
 
 module.exports = generateTestimonials;
