2c03ce3226274c6ed19d9247c158bf8ea27f8a5a
vbaobao <vbao.main@gmail.com>
2021-05-09 15:28:01 -0700

    try seeding without id

--- db/inserters/instructorsInserter.js
+++ db/inserters/instructorsInserter.js
@@ -7,12 +7,12 @@ const { InstructorsModel } = require('../models.js');
 
 const instructorsInsert = Promise.promisify(async (data, cb) => {
   try {
-    let maxID = await InstructorsModel.find().select('_id').sort([['_id', -1]]).limit(1);
-    maxID = maxID[0]?._id ? maxID[0]._id : 0;
-    Object.keys(data).forEach((key) => {
-      maxID++;
-      data[key]._id = maxID;
-    });
+    // let maxID = await InstructorsModel.find().select('_id').sort([['_id', -1]]).limit(1);
+    // maxID = maxID[0]?._id ? maxID[0]._id : 0;
+    // Object.keys(data).forEach((key) => {
+    //   maxID++;
+    //   data[key]._id = maxID;
+    // });
     const results = await InstructorsModel.insertMany(data);
     return cb(null, results);
   } catch (err) {
--- db/inserters/offeredBysInserter.js
+++ db/inserters/offeredBysInserter.js
@@ -7,12 +7,12 @@ const { OfferedBysModel } = require('../models.js');
 
 const offeredBysInsert = Promise.promisify(async (data, cb) => {
   try {
-    let maxID = await OfferedBysModel.find().select('_id').sort([['_id', -1]]).limit(1);
-    maxID = maxID[0]?._id ? maxID[0]._id : 0;
-    Object.keys(data).forEach((key) => {
-      maxID++;
-      data[key]._id = maxID;
-    });
+    // let maxID = await OfferedBysModel.find().select('_id').sort([['_id', -1]]).limit(1);
+    // maxID = maxID[0]?._id ? maxID[0]._id : 0;
+    // Object.keys(data).forEach((key) => {
+    //   maxID++;
+    //   data[key]._id = maxID;
+    // });
     const results = await OfferedBysModel.insertMany(data);
     return cb(null, results);
   } catch (err) {
