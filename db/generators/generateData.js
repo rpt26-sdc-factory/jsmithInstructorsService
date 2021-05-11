9301d9e64f0176d3710799b75321c5dca0baafc0
vbaobao <vbao.main@gmail.com>
2021-05-10 20:10:15 -0700

    delete unneeded file

--- db/generators/generateData.js
+++
@@ -1,9 +0,0 @@
-const generateInstructors = require('./instructorsGenerator');
-const generateOfferedBys = require('./offeredBysGenerator');
-const generateTestimonials = require('./testimonialsGenerator');
-
-module.exports = {
-  generateInstructors,
-  generateOfferedBys,
-  generateTestimonials,
-};
