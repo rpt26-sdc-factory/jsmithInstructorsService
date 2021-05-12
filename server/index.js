61eefe6ac82a1f0a8c460dd1653fd4529a1ce78b
vbaobao <vbao.main@gmail.com>
2021-05-12 16:14:25 -0700

    add dynamic pathing based on db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+/* eslint-disable global-require */
 /* eslint-disable no-console */
 /* eslint-disable radix */
 require('dotenv').config();
-require('../db/database.js');
 const app = require('./api.js');
+switch (process.env.NODE_ENV) {
+  case 'mongo':
+    require('../db/mongo/database.js');
+    break;
+  case 'cassandra':
+    require('../db/cassandra/database.js');
+    break;
+  default:
+    require('../db/mongo/database.js');
+    break;
+}
 
 const port = process.env.SERVER_PORT;
+const server = process.env.SERVER_HOST;
 
 app.listen(port, () => {
-  console.log(`Instructors service listening at http://localhost:${port}`);
+  console.log(`Instructors service listening at http://${server}:${port}`);
 });
