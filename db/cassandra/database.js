6413ac3a79d25aadf66ad9556928dd2e02125469
vbaobao <vbao.main@gmail.com>
2021-05-12 16:23:30 -0700

    debug config and add success console.log

--- db/cassandra/database.js
+++ db/cassandra/database.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-require('dotenv').configure();
+require('dotenv').config();
 const cass = require('cassandra-driver');
 
 const client = new cass.Client({
   contactPoints: [`${process.env.DB_HOST}:${process.env.CASSANDRA_PORT}`],
-  localDataCenter: 'test',
+  localDataCenter: 'datacenter1',
   keyspace: 'coursera',
 });
 
 client.connect()
-  .then((success) => console.log(success))
+  .then(() => console.log(`Connection to ${process.env.DB_HOST}:${process.env.CASSANDRA_PORT} has been successful!`))
   .catch((err) => console.error(err));
