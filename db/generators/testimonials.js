dd42b27e12d1dad8963f33cbf80cf7296705c6bc
vbaobao <vbao.main@gmail.com>
2021-05-14 23:27:20 -0700

    debug newlines

--- db/generators/testimonials.js
+++ db/generators/testimonials.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
@@ -8,32 +9,32 @@ const fs = require('fs');
 const batchSize = process.env.NODE_ENV === 'test' ? 100 : process.env.PRIMARY_RECORD_BATCH_SIZE;
 const numBatches = process.env.NODE_ENV === 'test' ? 1 : process.env.PRIMARY_RECORD_BATCH_NUM;
 
-const generateTestimonials = (courses) => {
+const generateTestimonials = (courses, batchNum) => {
   const testimonials = [];
 
   for (let courseNum = 1; courseNum <= courses; courseNum++) {
     const random = Math.floor(Math.random() * 5);
-    let testimonial = '';
+    const testimonial = [];
     for (let reviews = 0; reviews <= random; reviews++) {
-      testimonial += `${courseNum},${faker.name.firstName().concat(' ', faker.name.lastName().slice(0, 1), '.')},${faker.lorem.paragraph()}`;
+      testimonial.push(`${courseNum + (batchNum - 1) * courses},${faker.name.firstName().concat(' ', faker.name.lastName().slice(0, 1), '.')},${faker.lorem.sentence()}`);
     }
-    testimonials.push(testimonial);
+    testimonials.push(`\n${testimonial.join('\n')}`);
   }
 
   const stream = fs.createWriteStream('./db/seeders/testimonials.csv', { flags: 'a' });
-  for (let rows = 0; rows < courses; rows += 100000) {
-    const string = rows === 0 ? `\n${testimonials.splice(0, 100000).join('\n')}` : testimonials.splice(0, 100000).join('\n');
+  for (let rows = 0; rows < courses; rows += 1000) {
+    const string = testimonials.splice(0, 1000).join('');
     stream.write(string);
   }
   return testimonials;
 };
 
 const stream = fs.createWriteStream('./db/seeders/testimonials.csv', { flags: 'w' });
-stream.write('course_id,username,testimonial\n');
+stream.write('course_id,username,testimonial');
 let count = 1;
 const start = new Date();
 while (count <= numBatches) {
-  generateTestimonials(batchSize);
+  generateTestimonials(batchSize, count);
   count++;
 }
 const end = new Date();
