29db9b962cb25afd3a865b4d51d363c2cc34f341
vbaobao <vbao.main@gmail.com>
2021-06-04 11:57:32 -0700

    remove testimonials and offeredbys references

--- client/index.jsx
+++ client/index.jsx
@@ -2,11 +2,7 @@
 /* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Instructors from './components/instructors/Instructors.jsx';
-import OfferedBy from './components/offeredBy/OfferedBy.jsx';
-import Testimonials from './components/testimonials/Testimonials.jsx';
+import Instructors from './components/Instructors.jsx';
 import './styles.css';
 
 ReactDOM.render(<Instructors />, document.getElementById('instructors'));
-ReactDOM.render(<OfferedBy />, document.getElementById('offered-by'));
-ReactDOM.render(<Testimonials />, document.getElementById('testimonials'));
