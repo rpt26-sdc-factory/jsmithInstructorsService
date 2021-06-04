a43d23bc5e786c292720ebb2f7b7974c685ce5d8
vbaobao <vbao.main@gmail.com>
2021-06-04 12:04:58 -0700

    remove switch for other dbs

--- server/index.js
+++ server/index.js
@@ -2,21 +2,8 @@
 /* eslint-disable no-console */
 /* eslint-disable radix */
 require('dotenv').config();
+require('../db/postgres/database.js');
 const app = require('./api.js');
-switch (process.env.NODE_ENV) {
-  case 'mongo':
-    require('../db/mongo/database.js');
-    break;
-  case 'cassandra':
-    require('../db/cassandra/database.js');
-    break;
-  case 'postgres':
-    require('../db/postgres/database.js');
-    break;
-  default:
-    require('../db/mongo/database.js');
-    break;
-}
 
 const port = process.env.SERVER_PORT;
 const server = process.env.SERVER_HOST;
