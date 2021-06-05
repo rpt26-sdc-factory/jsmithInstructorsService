03cd9fbd9b51f552c46c21ed48c9275edd9fc472
vbaobao <vbao.main@gmail.com>
2021-06-05 10:15:41 -0700

    include env for deployment

--- db/postgres/database.js
+++ db/postgres/database.js
@@ -1,6 +1,12 @@
 const { Client } = require('pg');
 
-const client = new Client();
+const client = new Client({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
 
 client.connect();
 
