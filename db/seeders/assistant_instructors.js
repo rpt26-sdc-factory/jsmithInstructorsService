8fe5c6888e85104e13946e52a96f7c9427c36f0f
vbaobao <vbao.main@gmail.com>
2021-06-08 13:46:30 -0700

    combine generator and seeding into one

---
+++ db/seeders/assistant_instructors.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+require('dotenv').config();
+const client = require('../postgres/database.js');
+
+const batchSize = process.env.NODE_ENV === 'test' ? 100 : process.env.PRIMARY_RECORD_BATCH_SIZE;
+const numBatches = process.env.NODE_ENV === 'test' ? 1 : process.env.PRIMARY_RECORD_BATCH_NUM;
+
+const generateAssistantInstructors = (numCourses) => {
+  const courses = [];
+  for (let courseNumber = 1; courseNumber <= numCourses; courseNumber++) {
+    const numberOfAssistants = Math.floor(Math.random() * 4);
+    const assistants = [];
+
+    while (assistants.length < numberOfAssistants) {
+      const assistantIndex = Math.floor(Math.random() * numCourses) + 1;
+
+      // prevents the same instructor from being added twice to the same course
+      if (!assistants.includes(assistantIndex)) {
+        assistants.push(assistantIndex);
+      }
+    }
+    for (let i = 0; i < assistants.length; i++) {
+      courses.push({
+        course_id: courseNumber,
+        instructor_id: assistants[i],
+      });
+    }
+  }
+  return courses;
+};
+
+(async () => {
+  console.time('Insert 10 million courses\'s assistant instructors');
+  const assistants = generateAssistantInstructors(batchSize * numBatches);
+  const insertSize = 10000;
+  const numberInserts = Math.floor(assistants.length / insertSize);
+  for (let i = 0; i < numberInserts; i++) {
+    const pool = await client.connect();
+    const sql = 'INSERT INTO coursera.assistant_instructors (course_id,instructor_id) SELECT course_id,instructor_id FROM json_populate_recordset(NULL::coursera.assistant_instructors, $1)';
+    const option = [JSON.stringify(assistants.slice(i * insertSize, (i + 1) * insertSize))];
+    await pool.query(sql, option);
+    pool.release();
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`--- Inserted ${(i + 1) * insertSize} ---\r`);
+  }
+  console.timeEnd('Insert 10 million courses\'s assistant instructors');
+})();
---
+++ db/seeders/primary_instructors.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+require('dotenv').config();
+const client = require('../postgres/database.js');
+
+const batchSize = process.env.NODE_ENV === 'test' ? 100 : process.env.PRIMARY_RECORD_BATCH_SIZE;
+const numBatches = process.env.NODE_ENV === 'test' ? 1 : process.env.PRIMARY_RECORD_BATCH_NUM;
+
+const generatePrimaryInstructors = (numCourses) => {
+  const primaryInstructors = [];
+  for (let i = 1; i <= numCourses; i++) {
+    const instructorId = Math.floor(Math.random() * numCourses) + 1;
+    primaryInstructors.push({
+      course_id: i,
+      instructor_id: instructorId,
+    });
+  }
+  return primaryInstructors;
+};
+
+(async () => {
+  console.time('Insert 10 million primary instructors');
+  const instructors = generatePrimaryInstructors(batchSize * numBatches);
+  const insertSize = 10000;
+  const numberInserts = Math.floor(instructors.length / insertSize);
+  for (let i = 0; i < numberInserts; i++) {
+    const pool = await client.connect();
+    const sql = 'INSERT INTO coursera.primary_instructors (course_id,instructor_id) SELECT course_id,instructor_id FROM json_populate_recordset(NULL::coursera.primary_instructors, $1)';
+    const option = [JSON.stringify(instructors.slice(i * insertSize, (i + 1) * insertSize))];
+    await pool.query(sql, option);
+    pool.release();
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`--- Inserted ${(i + 1) * insertSize} ---\r`);
+  }
+  console.timeEnd('Insert 10 million primary instructors');
+})();
