d06880bb7750112ff1921799529e4491ce9696d7
vbaobao <vbao.main@gmail.com>
2021-06-08 13:46:04 -0700

    change client to pool

--- db/postgres/database.js
+++ db/postgres/database.js
@@ -1,13 +1,11 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
+const client = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
 });
-
-client.connect();
 
 module.exports = client;
