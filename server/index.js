ca259555f1073a84bcf8e4f3affb7f1609cf4cf6
vbaobao <vbao.main@gmail.com>
2021-06-10 13:19:58 -0700

    import @babel/register for SSR

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@
 /* eslint-disable no-console */
 /* eslint-disable radix */
 require('dotenv').config();
+require('@babel/register')({
+  ignore: [/(node_modules)/],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+});
 const app = require('./api.js');
 
 const port = process.env.SERVER_PORT;
