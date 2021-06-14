f5dc08c130b9b8ae54697777b58ad36582b8cbab
vbaobao <vbao.main@gmail.com>
2021-06-14 09:30:59 -0700

    config babel for ssr

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const client = {
   mode: 'development',
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
   output: {
@@ -15,6 +16,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime'],
         },
       },
       {
@@ -31,3 +33,68 @@ module.exports = {
     ignored: /node_modules/,
   },
 };
+
+const server = {
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: 'development',
+  entry: path.resolve(__dirname, 'server', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'server'),
+    filename: 'server.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+    ],
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
+};
+
+const Instructors = {
+  mode: 'development',
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom',
+  },
+  entry: path.resolve(__dirname, 'client', 'components/Instructors.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'client/components'),
+    filename: 'instructors.js',
+    library: 'instructors',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+    ],
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
+};
+
+module.exports = [client, server, Instructors];
