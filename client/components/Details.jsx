0c844e4e716a99948419fc9bf78918f8117e5856
vbaobao <vbao.main@gmail.com>
2021-06-14 21:16:57 -0700

    add optional params

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -19,13 +19,13 @@ const Details = (props) => {
       </div>
       <div className="instructor-learners">
         <svg className="instructor-learnersSVG" viewBox="0 0 80 80" height="18px" width="18px">
-          <path d={props.svgs.learnersSVG}></path>
+          <path d={props.svgs?.learnersSVG}></path>
         </svg>
         {`${props.instructor.learners} Learners`}
       </div>
       <div className="instructor-courses">
         <svg className="instructor-coursesSVG" viewBox="0 0 80 80" height="18px" width="18px">
-          <path d={props.svgs.coursesSVG}></path>
+          <path d={props.svgs?.coursesSVG}></path>
         </svg>
         {`${props.instructor?.courses?.length || 0} ${courses}`}
       </div>
