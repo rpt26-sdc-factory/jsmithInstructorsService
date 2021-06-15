9094b02c89cd9b9c28cd6ea86f71f42c4b3b3762
vbaobao <vbao.main@gmail.com>
2021-06-14 21:17:36 -0700

    create SSR routes

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ require('@babel/register')({
   ignore: [/(node_modules)/],
   presets: ['@babel/preset-env', '@babel/preset-react'],
 });
-const fs = require('fs');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 const Instructors = require('../client/components/Instructors.jsx').default;
 
 const port = process.env.SERVER_PORT;
@@ -25,7 +25,8 @@ const {
   getPrimaryInstructor,
   setInstructor,
   deleteInstructor,
-} = require('./controllers/controllers.js');
+} = require('./controllers.js');
+const serverSideRender = require('./serverSideRender.js');
 
 const app = express();
 
@@ -37,13 +38,33 @@ app.use('/instructors.js', express.static(path.join(__dirname, '../client/compon
 app.use(`/${process.env.LOADERIO}.txt`, express.static(path.join(__dirname, `../${process.env.LOADERIO}.txt`)));
 app.use(cors());
 
-app.get('/:courseNumber', (req, res) => {
-  fs.readFile(path.resolve('./public/index.html'), 'utf8', (err, data) => {
-    if (err) return res.status(500).send('Page could not be loaded!');
-    const component = React.createElement(Instructors, { 'course': req.params.courseNumber });
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+app.get('/:courseNumber', async (req, res) => {
+  try {
+    const props = await serverSideRender(req);
+    console.log('regular get request: ', req.params)
+    fs.readFile(path.resolve('./public/index.html'), 'utf8', (err, data) => {
+      if (err) return res.status(500).send('Page could not be loaded!');
+      const component = React.createElement(Instructors, props);
+      const rendered = ReactDOMServer.renderToString(component);
+      res.end(data.replace('<div id="instructors"></div>', `<div id="instructors">${rendered}</div>`));
+    });
+  } catch (err) {
+    res.status(500).send(err)
+  }
+});
+
+app.get('/:courseNumber/innerHTML', async (req, res) => {
+  console.log('innerHTML: ', req.params);
+  try {
+    const props = await serverSideRender(req);
+    const component = React.createElement(Instructors, props);
     const rendered = ReactDOMServer.renderToString(component);
-    return res.end(data.replace('<div id="instructors"></div>', `<div id="instructors">${rendered}</div>`));
-  });
+    res.send(rendered);
+  } catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 app.post('/api/instructors', createInstructors);
