00cf84e8500758ba22df124c36b3c39c93d0dde9
vbaobao <vbao.main@gmail.com>
2021-06-15 18:01:34 -0700

    set up loaderio endpoint

---
+++ __stress_tests__/loaderio_postRequests.js
@@ -0,0 +1,11 @@
+module.exports = (req, res) => {
+  const getRandom = (max) => Math.floor(Math.random() * max);
+  const max = 10000001;
+  let endpoint = getRandom(max);
+
+  while (endpoint < max * 0.9) {
+    endpoint = getRandom(max);
+  }
+
+  res.redirect(307, `/${endpoint}`);
+};
